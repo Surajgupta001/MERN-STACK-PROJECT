aa55ce20c328969453f5d357ded54d903e81dc12
Suraj Gupta <surajgupta7070031833@gmail.com>
2025-10-07 20:20:21 +0530

    adding user routing and clerk middleware to server configuration

--- social-media-app/Backend/server.js
+++ social-media-app/Backend/server.js
@@ -4,6 +4,8 @@ import 'dotenv/config';
 import connectDB from './config/database.js';
 import { inngest, functions } from './inngest/index.js';
 import { serve } from "inngest/express";
+import { clerkMiddleware } from '@clerk/express'
+import userRouter from './routes/user.routes.js';
 
 // App config
 const app = express();
@@ -14,6 +16,7 @@ await connectDB();
 // Middlewares
 app.use(express.json());
 app.use(cors());
+app.use(clerkMiddleware())
 
 app.get('/', (req, res) => {
     res.send('Server is running...');
@@ -23,6 +26,8 @@ app.use("/api/inngest", serve({
     client: inngest, 
     functions
 }));
+
+app.use('/api/user', userRouter);
 
 const port = process.env.PORT || 8000;
 
