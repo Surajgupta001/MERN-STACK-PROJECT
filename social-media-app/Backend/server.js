bc60a78d6b5a02a11f901e9817d366dfc98b21b1
Suraj Gupta <surajgupta7070031833@gmail.com>
2025-10-12 14:09:37 +0530

    initialize server with Express and Inngest integration

---
+++ social-media-app/Backend/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './config/database.js';
+import { serve } from "inngest/express";
+import { inngest, functions } from './inngest/index.js';
+
+
+const app = express();
+
+await connectDB();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Social Media App API');
+});
+
+// Set up the "/api/inngest" (recommended) routes with the serve handler
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
