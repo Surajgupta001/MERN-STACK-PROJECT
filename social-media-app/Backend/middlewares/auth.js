24a15eabc5a34249d952712a3633dfc64caba3fe
Suraj Gupta <surajgupta7070031833@gmail.com>
2025-10-13 02:16:59 +0530

    Add authentication middleware to protect routes

---
+++ social-media-app/Backend/middlewares/auth.js
@@ -0,0 +1,22 @@
+export const protect = async (req, res, next) => {
+    try {
+        const { userId } = await req.auth();
+        if (!userId) {
+            return res
+            .status(401)
+            .json({
+                success: false,
+                message: 'Not authorized' 
+            });
+        }
+        next();
+    } catch (error) {
+        console.error('Auth Middleware Error:', error);
+        return res
+        .status(401)
+        .json({
+            success: false,
+            message: error.message
+        });
+    }
+};
