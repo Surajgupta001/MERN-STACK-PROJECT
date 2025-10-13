9bb7f877ef9ad07c871c941457f803988b63bb3d
Suraj Gupta <surajgupta7070031833@gmail.com>
2025-10-14 00:55:06 +0530

    Import and use user routes in the server setup

--- social-media-app/Backend/server.js
+++ social-media-app/Backend/server.js
@@ -5,6 +5,7 @@ import connectDB from './config/database.js';
 import { serve } from "inngest/express";
 import { inngest, functions } from './inngest/index.js';
 import { clerkMiddleware } from '@clerk/express'
+import userRouter from './routes/userRoutes.js';
 
 const app = express();
 
@@ -20,6 +21,9 @@ app.get('/', (req, res) => {
 
 // Set up the "/api/inngest" (recommended) routes with the serve handler
 app.use("/api/inngest", serve({ client: inngest, functions }));
+
+// Import and use routes
+app.use('/api/user', userRouter);
 
 const port = process.env.PORT || 5000;
 
