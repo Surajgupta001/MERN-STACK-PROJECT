f36a43bf021ff1fe5f3603542a13d514bd5426e2
Suraj Gupta <surajgupta7070031833@gmail.com>
2026-05-07 20:35:53 +0530

    feat: configure nodemailer transporter and implement email sending utility

--- employee-management-system/Backend/config/nodemailer.js
+++ employee-management-system/Backend/config/nodemailer.js
@@ -2,23 +2,23 @@ import { createTransport } from "nodemailer";
 
 // Create a transporter using SMTP
 const transporter = createTransport({
-    host: "smtp-relay.brevo.com",
-    port: 587,
-    auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
-    },
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
 });
 
 const sendEmail = async ({ to, subject, body }) => {
-    const response = await transporter.sendMail({
-        from: process.env.SENDER_EMAIL,
-        to,
-        subject,
-        html: body,
-    })
+  const response = await transporter.sendMail({
+    from: process.env.SENDER_EMAIL || "surajgupta7070031833@gmail.com",
+    to,
+    subject,
+    html: body,
+  });
 
-    return response;
+  return response;
 };
 
 export default sendEmail;
